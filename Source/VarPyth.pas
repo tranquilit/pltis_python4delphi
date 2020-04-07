87f23bbd9b2cc401edd5e8ef38e4d4ca04c033b4
Octave Larose <olarose@tranquil.it>
2020-04-07 10:49:51 +0200

    TIS hacks

--- Source/VarPyth.pas
+++ Source/VarPyth.pas
@@ -45,6 +45,9 @@ uses
 type
   TSequenceType = (stTuple, stList);
 
+const
+  NOARGS='--noarg--';
+
 { Python variant creation utils }
 function VarPythonCreate( AObject : PPyObject ) : Variant; overload;
 function VarPythonCreate( const AValue : Variant ) : Variant; overload;
@@ -154,7 +157,8 @@ type
   {$IFEND}
 {$ENDIF}
 {$IF DEFINED(FPC_FULLVERSION) and (FPC_FULLVERSION >= 20500)}
-  {$DEFINE USESYSTEMDISPINVOKE}
+//  {$DEFINE USESYSTEMDISPINVOKE} // Hack Tranquil IT : to allow the hack code to be executed
+  {$DEFINE PATCHEDSYSTEMDISPINVOKE}
 {$IFEND}
 
   { Python variant type handler }
@@ -1242,9 +1246,11 @@ procedure TPythonVariantType.DispInvoke(Dest: PVarData;
           // property get or function with 0 argument
           else if LArgCount = 0 then
           begin
-            if not GetProperty(Dest^, Source, LIdent) and
-               not DoFunction(Dest^, Source, LIdent, VarParams) then
-              RaiseDispError;
+			//hack Tranquil IT ... first DoFunction then GetProperty if '--noargs--' is passed as first parameter
+        	if not (docall and DoFunction(Dest^, Source, string(LIdent), LArguments)) and
+          	not GetProperty(Dest^, Source, string(LIdent))
+          	and not DoFunction(Dest^, Source, string(LIdent), LArguments) then
+          	RaiseDispError;
           end
 
           // function with N arguments
@@ -1362,6 +1368,8 @@ var
       LNamePtr := LNamePtr + Succ(StrLen(LNamePtr));
       fNamedParams[I-LNamedArgStart].Index := I;
       fNamedParams[I-LNamedArgStart].Name  := AnsiString(LNamePtr);
+	  //Tranquil IT Hack : named params in lowercase, the pascal compiler switches all named params to uppercase by default
+	  fNamedParams[I-LNamedArgStart].Name  := lowercase(AnsiString(LNamePtr));
     end;
 
     // error is an easy expansion
@@ -1439,6 +1447,8 @@ var
     else
     begin
       LArguments[I].VType := LArgType;
+      WriteLn(LArgType); // TODO remove
+      WriteLn(IntToStr(CVarTypeToElementInfo[LArgType].Size)); // TODO remove
       case CVarTypeToElementInfo[LArgType].Size of
         1, 2, 4:
         begin
@@ -1461,6 +1471,7 @@ var
   I, LArgCount: Integer;
   LIdent: AnsiString;
   LTemp: TVarData;
+  docall:Boolean;
 begin
   //------------------------------------------------------------------------------------
   // Note that this method is mostly a copy&paste from  TInvokeableVariantType.DispInvoke
@@ -1486,6 +1497,17 @@ begin
   SetLength(LStrings, LArgCount);
   for I := 0 to LArgCount - 1 do
     ParseParam(I);
+
+  // Hack Tranquil IT to call a python method with no arguments, we use a s'well known' string argument --noargs--
+  //   because pascal compiler doesn't make difference between access to a property and call of method without argument
+  if (LArgCount=1) and (LArguments[0].VType = varOleStr) and (Widestring(LArguments[0].VSTRING)=NOARGS) then
+      begin
+          LArgCount:=0;
+          SetLength(LArguments,0);
+          docall:=True;
+     end
+  else
+      docall :=False;
 
   // What type of invoke is this?
   case CallDesc^.CallType of
