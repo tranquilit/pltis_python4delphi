b60ed1e540689aaa51e7bedce474d59d7b8c066a
htouvet <htouvet@None>
2021-06-22 10:34:49 +0200

    Use 64bit TLibHandle for python module handle (for linux modules)
    Add Py_buffer and PyBuffer_Release interface

--- Source/PythonEngine.pas
+++ Source/PythonEngine.pas
@@ -1239,6 +1239,52 @@ type
 //#######################################################
   PyGILState_STATE = (PyGILState_LOCKED, PyGILState_UNLOCKED);
 
+
+type
+  Py_ssize_t = LongInt;
+  PPy_ssize_t = ^LongInt;
+
+  Py_buffer=record
+    //A pointer to the start of the logical structure described by the buffer fields. This can be any location within the underlying physical memory block of the exporter. For example, with negative strides the value may point to the end of the memory block.
+    //For contiguous arrays, the value points to the beginning of the memory block.
+    buf: Pointer;
+
+    //A new reference to the exporting object. The reference is owned by the consumer and automatically decremented and set to NULL by PyBuffer_Release(). The field is the equivalent of the return value of any standard C-API function.
+    //As a special case, for temporary buffers that are wrapped by PyMemoryView_FromBuffer() or PyBuffer_FillInfo() this field is NULL. In general, exporting objects MUST NOT use this scheme.
+    obj: PPyObject;
+    len: Py_ssize_t;
+    //An indicator of whether the buffer is read-only. This field is controlled by the PyBUF_WRITABLE flag.
+    readonly: Integer;
+    //Item size in bytes of a single element. Same as the value of struct.calcsize() called on non-NULL format values.
+    //Important exception: If a consumer requests a buffer without the PyBUF_FORMAT flag, format will be set to NULL, but itemsize still has the value for the original format.
+    //If shape is present, the equality product(shape) * itemsize == len still holds and the consumer can use itemsize to navigate the buffer.
+    //If shape is NULL as a result of a PyBUF_SIMPLE or a PyBUF_WRITABLE request, the consumer must disregard itemsize and assume itemsize == 1.
+    itemsize: Py_ssize_t;
+    //A NUL terminated string in struct module style syntax describing the contents of a single item. If this is NULL, "B" (unsigned bytes) is assumed.
+    //This field is controlled by the PyBUF_FORMAT flag.
+    format: PAnsiChar;
+    //The number of dimensions the memory represents as an n-dimensional array. If it is 0, buf points to a single item representing a scalar. In this case, shape, strides and suboffsets MUST be NULL.
+    //The macro PyBUF_MAX_NDIM limits the maximum number of dimensions to 64. Exporters MUST respect this limit, consumers of multi-dimensional buffers SHOULD be able to handle up to PyBUF_MAX_NDIM dimensions.
+    ndim: Integer;
+    //An array of Py_ssize_t of length ndim indicating the shape of the memory as an n-dimensional array. Note that shape[0] * ... * shape[ndim-1] * itemsize MUST be equal to len.
+    //Shape values are restricted to shape[n] >= 0. The case shape[n] == 0 requires special attention. See complex arrays for further information.
+    //The shape array is read-only for the consumer.
+    shape: ^Py_ssize_t;
+    //An array of Py_ssize_t of length ndim giving the number of bytes to skip to get to a new element in each dimension.
+    //Stride values can be any integer. For regular arrays, strides are usually positive, but a consumer MUST be able to handle the case strides[n] <= 0. See complex arrays for further information.
+    //The strides array is read-only for the consumer.
+    strides: ^Py_ssize_t;
+    //An array of Py_ssize_t of length ndim. If suboffsets[n] >= 0, the values stored along the nth dimension are pointers and the suboffset value dictates how many bytes to add to each pointer after de-referencing. A suboffset value that is negative indicates that no de-referencing should occur (striding in a contiguous memory block).
+    //If all suboffsets are negative (i.e. no de-referencing is needed), then this field must be NULL (the default value).
+    //This type of array representation is used by the Python Imaging Library (PIL). See complex arrays for further information how to access elements of such an array.
+    //The suboffsets array is read-only for the consumer.
+    suboffsets: ^Py_ssize_t;
+    //This is for use internally by the exporting object. For example, this might be re-cast as an integer by the exporter and used to store flags about whether or not the shape, strides, and suboffsets arrays must be freed when the buffer is released. The consumer MUST NOT alter this value.
+    internal: Pointer;
+  end;
+  PPy_buffer=^Py_buffer;
+
+
 //#######################################################
 //##                                                   ##
 //##         New exception classes                     ##
@@ -1483,7 +1529,7 @@ type
     FAutoUnload         : Boolean;
     FFatalMsgDlg        : Boolean;
     FFatalAbort         : Boolean;
-    FDLLHandle          : THandle;
+    FDLLHandle          : TLibHandle; // 64 bits !
     FUseLastKnownVersion: Boolean;
     FOnBeforeLoad       : TNotifyEvent;
     FOnAfterLoad        : TNotifyEvent;
@@ -1976,6 +2022,8 @@ type
     PyErr_SetInterrupt              : procedure; cdecl;
     PyGILState_Ensure               : function() : PyGILstate_STATE; cdecl;
     PyGILState_Release              : procedure(gilstate : PyGILState_STATE); cdecl;
+    PyBuffer_Release                : procedure(view: PPy_buffer); cdecl;
+
 {Further exported Objects, may be implemented later}
 {
     PyCode_New: Pointer;
@@ -3338,6 +3386,22 @@ end;
 (**            class TDynamicDll                      **)
 (**                                                   **)
 (*******************************************************)
+{$ifdef POSIX}
+function GetModulePath(Address: Pointer): String;
+const
+  Dummy: Boolean = False;
+var
+  dlinfo: dl_info;
+begin
+  if Address = nil then Address:= @Dummy;
+  FillChar({%H-}dlinfo, SizeOf(dlinfo), #0);
+  if dladdr(Address, @dlinfo) = 0 then
+    Result:= EmptyStr
+  else begin
+    Result:= dlinfo.dli_fname;
+  end;
+end;
+{$endif}
 
 procedure TDynamicDll.DoOpenDll(const aDllName : string);
 begin
@@ -3353,7 +3417,7 @@ begin
       {$ENDIF});
     {$ELSE}
     //Linux: need here RTLD_GLOBAL, so Python can do "import ctypes"
-    FDLLHandle := THandle(dlopen(PAnsiChar(AnsiString(GetDllPath+DllName)),
+    FDLLHandle := TLibHandle(dlopen(PAnsiChar(AnsiString(GetDllPath+DllName)),
       RTLD_LAZY+RTLD_GLOBAL));
     {$ENDIF}
   end;
@@ -4107,6 +4171,8 @@ begin
   PyErr_SetInterrupt       := Import('PyErr_SetInterrupt');
   PyGILState_Ensure        := Import('PyGILState_Ensure');
   PyGILState_Release       := Import('PyGILState_Release');
+
+  PyBuffer_Release         := Import('PyBuffer_Release');
 end;
 
 function TPythonInterface.Py_CompileString( s1,s2:PAnsiChar;i:integer):PPyObject; cdecl;
